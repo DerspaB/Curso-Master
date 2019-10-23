e0c8ac99672023cba28fa42e5d931d94032077b5
Jorge <jun-beto155@hotmail.com>
2019-10-23 16:47:07 -0500

    Cursooo

--- js/29-Dom.js
+++ js/29-Dom.js
@@ -4,13 +4,46 @@
 // querySelector tambien permite seleccionar un elemento de una pagina HTML
 
 
-function cambiacolor(color){
-    caja.style.background = color;
+var caja = document.querySelector('#micaja');
+caja.innerHTML = "Texto en la caja desde JS";
+caja.style.background = "red";
+caja.style.padding = "20px";
+caja.style.color = "white";
+caja.className = "hola hola2";
+
+// conseguir todos los elementos por su etiqueta
+var todosLosDivs = document.getElementsByTagName('div');
+
+var valor;
+for (valor in todosLosDivs) {
+    if (typeof(todosLosDivs[valor].textContent) == 'string') {
+        var parrafo = document.createElement("p");
+        var texto = document.createTextNode(todosLosDivs[valor].textContent);
+        parrafo.appendChild(texto);
+        document.querySelector('#miseccion').appendChild(parrafo);
+    }
 }
 
- var caja = document.querySelector('#micaja');
- caja.innerHTML = "Hola people jaja xD";
- caja.style.color = "white";
- caja.style.padding = "20px";
- caja.style.background = "red";
- console.log(caja);
+// Conseguir elementos por su clase css
+var divsRojos = document.getElementsByClassName('rojo');
+var amarillos = document.getElementsByClassName('amarillo');
+amarillos[0].style.background = 'yellow';
+
+var div;
+for (div in divsRojos) {
+    if (divsRojos[div].className == 'rojo') {
+        divsRojos[div].style.background = "green";
+        divsRojos[div].style.color = "white";
+        console.log(divsRojos[div]);
+    }
+
+
+}
+
+// Query Selector
+
+var id = document.querySelector("#encabezado");
+console.log(id);
+
+var claseRojo = document.querySelector("div.rojo");
+console.log(claseRojo);
