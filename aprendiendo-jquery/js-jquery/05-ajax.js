e7a57fdcf13a321eb6afab405ceaa85f5f618d50
Jorge <jun-beto155@hotmail.com>
2019-12-04 14:55:33 -0500

    Efectos jQuery UI

--- aprendiendo-jquery/js-jquery/05-ajax.js
+++ aprendiendo-jquery/js-jquery/05-ajax.js
@@ -11,17 +11,47 @@ $(document).ready(function(){
         });
     });
 
-
+    // e captura el evento 
     $("#formulario").submit(function(e){
         e.preventDefault();
 
         var usuario = {
-        name: $('input[name="name"]').val(),
+        name: $('#name').val(),
         last_name: $('input[name="web"]').val()
         };
-        $.post($(this).attr("action"),usuario,function(response){
-        console.log(response);
+        // $.post($(this).attr("action"),usuario,function(response){
+        // console.log(response);
+        
+        // }).done(function(){
+        //     alert("Usuario Añadido correctamente");
+        // });
+
+
+        //Parametros necesarios para realizar $.ajax:
+            // type = es la manera en la que se va a enviar los datos (POST Y GET)
+            // url = es la direcciona a donde se van a enviar los datos
+            // data = es la informacion que se va a enviar 
+        // beforeSend: Que antes de que se envie realize alguna funcion
+        //succes: Si todo es correcto que haga algo
+        // error: Si ocurre un error que haga algo
+        // timeout lo que se quiere que se demore
+        $.ajax({
+            type: 'POST',
+            url: $(this).attr("action"),
+            data: usuario,
+            beforeSend: function(){
+                console.log("Enviando Usuario....");
+            },
+            success: function(response){
+                console.log(response);
+            },
+            error: function(){
+                console.log("A ocurrido un errror");
+            },
+            timeout: 2000
+
         });
+
         return false;
 
     });
---
+++ aprendiendo-jquery/js-jquery/06-interface.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    // Mover un elemento por la pagina (draggable)
+    $(".elemento").draggable();
+
+    // redimensionar
+
+    $(".elemento").resizable();
+
+    // Seleccionar elementos
+    // $(".lista-seleccionable").selectable();
+
+    // Ordenar elementos
+    $(".lista-seleccionable").sortable();
+
+    // drop
+    $("#elemento-movido").draggable();
+    $("#area").droppable({
+        drop: function(){
+            alert("entro xD");
+        }
+    });
+
+    // Efectos
+    $("#mostrar").click(function(){
+        $('.caja-efectos').toggle("shake");
+    });
+
+    // Tooltip
+    $(document).tooltip();
+});
