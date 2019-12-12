317e739c8a63224840979cb1a73fd4f50d9fcf46
Jorge <jorgeparrado.cos@gmail.com>
2019-12-12 11:53:15 -0500

    cambiaColor

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -49,9 +49,60 @@ var post = `
         <a href="#" class="button-more">Leer más</a>
     </article>
 `;
-console.log(post);
-$("#posts").append(post);
+$("#posts").append(post );
 });
+
+// Selector de tema
+
+var theme = $("#theme");
+$("#to-green").click(function(){
+    theme.attr("href","css/green.css");
+
+});
+$("#to-blue").click(function(){
+    theme.attr("href","css/blue.css");
+
+});
+$("#to-red").click(function(){
+    theme.attr("href","css/red.css");
+
+});
+
+$("#yo-green").hover(function(){
+    $(document).attr("cursor","pointer");
+})
+
+// Scroll arriba de la web
+$('.subir').click(function(e){
+    e.preventDefault();
+    $('html,body').animate({
+        scrollTop:0
+
+
+    },1200);
+    return false;
+});
+
+// login falso
+$('#login form').submit(function(){
+    var form_name = $('#form_name').val();
+    localStorage.setItem("form_name", form_name);
+
+});
+var form_name = localStorage.getItem("form_name");
+
+if(form_name!= null && form_name != "undefined"){
+    var about_parrafo =  $('#about p')
+    about_parrafo.html("<br><strong>Bienvenido, "+form_name+"</strong>");
+    about_parrafo.append("<a href='#' id='logout'> Cerrar Sesión</a>")
+    $('#login').hide();
+
+    $('#logout').click(function(){
+        localStorage.clear();
+        location.reload();
+    })
+}
+
 
 });
 
